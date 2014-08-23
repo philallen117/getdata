76985525479e5ec17b38f7c2c9e42fd746163d03
philallen117 <phil.allen.gm@gmail.com>
2014-08-23 18:49:01 +0100

    load tidy data

--- README.md
+++ README.md
@@ -11,22 +11,47 @@ One strand of that is to infer physical activity states from movement sensors. F
 Focus of data preparation
 -------------------------
 
-Prepare data to facilitate the recognition of phyiscal activity states from smartphone accelerometers and gyroscopes. The recognition work could be attempted using machine learning techniques, for example.
-In machine learning, some practitioners offer the advice to start simple. Therefore, this data preparation includes just a few sensor variables.
+Prepare data to facilitate the recognition of phyiscal activity states from smartphone accelerometers and gyroscopes (which can be used to estimate angular acceleration). The recognition work could be attempted using machine learning techniques, for example.
+
+In machine learning, some practitioners offer the advice to start simple. Therefore, this data preparation includes just a few variables.
+
+The publishers of the data have already processed the raw sensor output, and I have chosen to exoloit that. I have decided to extract the "Body" components (with estimated gravity constributions subtracted out).
+
+Additionally, I have chosen to focus on the simple metrics in the time domain. Without a direct request from somebody who understands the frequency domain measures and knows what they plan to do with the frequency domain data, I have chosen to extract only data whose derivation I understand. Boring, but morally defensible.
+
+That is, I have concentrated on
 
 Source data
 -----------
 
 The source data is a given by the terms of the assignment.
+
 The unpacked source data contains test and training data. I have processed only the training data.
-The 
+
+mean and standard deviation.
+
+
+
+Deliverables
+------------
+
+* [Code table](./code_table.html) 
+
+* [Source for code table](./code_table.Rmd)
+
+* [Data preparation script](./runanalysis.R) 
+
+
+
 
 Use of tools
 ------------
 
-The [code table document](./code_table.html) was prepared using R Markdown, which is supported by RStudio. Here is the [source](./code_table.Rmd)
-R v3.1.1 and RStudio v0.98 were used.
-Packages used in the [data preparation script](./runanalysis.R) can be seen from library calls in the source.
+* R v3.1.1 and RStudio v0.98 were used.
+
+* The [code table document](./code_table.html) was prepared using R Markdown, which is supported by RStudio.
+
+* Packages used in the [data preparation script](./runanalysis.R) can be seen from library calls.
 
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -11,5 +11,69 @@ if(!file.exists(raw.file) {
 	unzip(raw.file, exdir = "./data")
 }
 
+top.dir <- "./data/UCI HAR Dataset" # location of activity labels, feature labels, etc
 train.dir <- "./data/UCI HAR Dataset/train" # location of training data
+test.dir <- "./data/UCI HAR Dataset/test" # location of test data
+
+## Activity labels: function as a lookup for data files.
+act.labels <- read.table(paste(top.dir, "activity_labels.txt", sep = "/"))
+names(act.labels) <- c("actid", "activity")
+## Remove non-alpha chars
+act.labels$code <- gsub("_", "", act.labels$code)
+
+## Names of derived sensor measures ("features")
+sensor.varnames <- read.table(paste(top.dir, "features.txt", sep = "/"))
+sensor.varnames <- tolower(gsub("()", "", sensor.varnames[,2], fixed = TRUE))
+mean.std.names <- sensor.varnames[grepl("mean", sensor.varnames) | grepl("std", sensor.varnames)]
+
+
+library(plyr)
+
+## Read and decode the activity labels for the train observations
+## Note that join {plyr} is order-preserving in its first parameter
+train.activities <- read.table(paste(train.dir, "y_train.txt", sep = "/"))
+names(train.activities) <- c("actid")
+train.activities <- join(train.activities, act.labels, by = "actid", type="inner")
+train.activities <- train.activities[c("activity")]
+
+## Read the participants for the train observations
+train.parts <- read.table(paste(train.dir, "subject_train.txt", sep = "/"))
+names(train.parts) <- c("participant")
+
+## Read the derived sensor measures for the train observations
+train.sensors <- read.table(paste(train.dir, "X_train.txt", sep = "/"))
+names(train.sensors) <- sensor.varnames
+## Reduce to measures that mention mean and standard deviation
+train.sensors <- train.sensors[mean.std.names]
+
+## Compose train data by observation.
+train.data <- cbind(train.activities, train.parts, train.sensors)
+
+## Read and decode the activity labels for the test observations
+## Note that join {plyr} is order-preserving in its first parameter
+test.activities <- read.table(paste(test.dir, "y_test.txt", sep = "/"))
+names(test.activities) <- c("actid")
+test.activities <- join(test.activities, act.labels, by = "actid", type="inner")
+test.activities <- test.activities[c("activity")]
+
+## Read the participants for the test observations
+test.parts <- read.table(paste(test.dir, "subject_test.txt", sep = "/"))
+names(test.parts) <- c("participant")
+
+## Read the derived sensor measures for the test observations
+test.sensors <- read.table(paste(test.dir, "X_test.txt", sep = "/"))
+names(test.sensors) <- sensor.varnames
+## Reduce to measures that mention mean and standard deviation
+test.sensors <- test.sensors[mean.std.names]
+
+## Compose test data by observation.
+test.data <- cbind(test.activities, test.parts, test.sensors)
+
+all.data <- rbind(test.data, train.data)
+
+simple <- all.data[c("")]
+
+means <- 5
+
+write.table(all.data, file = "./data/out.txt", sep = ",", col.names = TRUE)
 
